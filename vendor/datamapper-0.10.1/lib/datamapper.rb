60328b1c40fd33c8fdf17c39ca3d9b914415c953
Dan Nuttall <pixelblend@gmail.com>
2010-02-02 01:02:28 +0000

    learning to erase stuff correctly in git

--- vendor/datamapper-0.10.1/lib/datamapper.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-
-DEPENDENCIES = %w[ dm-core dm-aggregates dm-constraints dm-migrations dm-serializer dm-timestamps dm-validations dm-types ]
-
-DEPENDENCIES.each do |lib|
-  gem lib, '0.10.1'
-  require lib
-end
