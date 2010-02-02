11e187ed01e2d1eb9613c0bf096a5134c6a1f949
Dan Nuttall <pixelblend@gmail.com>
2010-02-02 09:32:58 +0000

    added vendored gems to test

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,6 @@
 ENV['RACK_ENV'] = 'test'
 ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+Dir[File.join ROOT, "vendor/*/lib"].each { |path| $:.unshift path } 
 
 require 'cucumber/formatter/unicode'
 require 'lib/disharmony'
